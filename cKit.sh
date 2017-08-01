c0536d860b1bcefb4f5b98b0be4c6aa4a06ac121
Kalin <kalin.dimitrov@paragon.net.uk>
2017-08-01 18:49:24 +0300

    Implemented the DDoS menu

--- cKit.sh
+++ cKit.sh
@@ -1,4 +1,4 @@
-W#!/bin/bash
+#!/bin/bash
 
 ###################
 ###  Variables  ###
@@ -300,6 +300,60 @@ Exitmenu(){
         exit 1
 }
 
+##
+# Function that lists the Ips which are connected to server and how many connections exist from each IP
+# You can easy it in case of severe traffic going to the server
+##
+function ActiveConn(){
+	netstat -anp |grep 'tcp\|udp' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -n
+DDoSMenu
+}
+
+##
+# Function that lists the users which are running the most processes at the moment - the top 5 users
+# You can use it in order to determinate which user is giving the spike on the server's load
+##
+function TopUsers(){
+	ps aux | awk '{print $1}' | sort | uniq -c | sort -nk1 | tail -n5
+DDoSMenu
+}
+
+##
+# Function that lists the total process running by the users
+# You can use to see all users which are running processes at the moment
+##
+function AllUsers(){
+	ps aux | awk '{print $1}' | sort | uniq -c | sort -nk1
+DDoSMenu
+}
+
+##
+# Function that shows the % CPU usage at the moment
+# You can use it in order to determinate if the current traffic is causingg too much CPU usage
+##
+function CurrentCPUusage(){
+	grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+DDoSMenu
+}
+
+##
+# Function that lists all the active connections for a specific port defined by the script user
+# You can use the option to see the traffic for every port you need
+##
+function GetPortConn(){
+	unset specport
+	while [ -z $specport ]; do
+        echo "Please enter the desired port:"
+	read specport
+        done
+        if [ $specport = "exit" ]; then
+	DDoSMenu
+	else
+        netstat -plan | grep :$specport | awk {'print $5'} | cut -d: -f 1 | sort | uniq -c | sort -nk 1
+	fi
+       	DDoSMenu
+}
+
 ###########################
 ###  Quick Access Menu ###
 ###########################
@@ -424,6 +478,30 @@ $(ColorBlue 'Choose an option:') "
         esac
 }
 
+DDoSMenu(){
+                ColorGreen "        "
+echo -ne "
+Main Menu
+$(ColorGreen '1)') Lists the Ips which are connected to server and how many connections exist from each IP
+$(ColorGreen '2)') lists the users which are running the most processes at the moment - the top 5 users
+$(ColorGreen '3)') Function that lists the total process running by the users
+$(ColorGreen '4)') Function that shows the % CPU usage at the moment
+$(ColorGreen '5)') Function that lists all the active connections for a specific port defined by the script user
+$(ColorGreen '0)') Back To Main Menu
+
+$(ColorBlue 'Choose an option:') "
+                read a
+                case $a in
+		1) ActiveConn;;
+                2) TopUsers;;
+                3) AllUsers;;
+                4) CurrentCPUusage;;
+                5) GetPortConn;;
+                0) MainMenu;;
+                *) echo -e $red"Wrong command."$clear; WrongCommand;;
+        esac
+}
+
 #################
 # The Main Menu #
 #################
@@ -435,7 +513,8 @@ Main Menu
 $(ColorGreen '1)') Access Logs Menu
 $(ColorGreen '2)') SPAM Scan Menu
 $(ColorGreen '3)') MySQL Menu
-$(ColorGreen '4)') Handy Tools
+$(ColorGreen '4)') DDoS Menu
+$(ColorGreen '5)') Handy Tools
 $(ColorGreen '0)') Exit
 
 $(ColorBlue 'Choose an option:') "
@@ -444,7 +523,8 @@ $(ColorBlue 'Choose an option:') "
                 1) MenuAcess;;
 		2) EmailsMenu;;
 		3) MySQLMenu;;
-		4) ToolsMenu;;
+		4) DDoSMenu;;
+		5) ToolsMenu;;
 		0) Exitmenu;;
 		*) echo -e $red"Wrong command."$clear; WrongCommand;;
         esac
