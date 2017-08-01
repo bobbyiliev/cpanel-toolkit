db39584c2b2f1826aff419aa15d5de15aedd0503
root <root@bobby.servers.prgn.misp.co.uk>
2017-08-01 09:39:47 +0000

    Fixing an issue with the function where the var was not being set to int

--- scripts/mysql_sleeping.sh
+++ scripts/mysql_sleeping.sh
@@ -8,7 +8,8 @@ function kill_mysql_sleeping_proc() {
         else
             	for i in $(mysql -e 'show processlist' | grep 'Sleep' | awk '{print $1}'); do
                         prockilled=0
-                        sleeptime=$(mysqladmin proc | grep $i | grep -v '\-\-' | grep -v 'Time' | awk -F'|' '{ print $7 }' | sed 's/ //g');
+                        sleeptime=$(mysqladmin proc | grep "\<$i\>" | grep -v '\-\-' | grep -v 'Time' | awk -F'|' '{ print $7 }' | sed 's/ //g' | tail -1);
+                        sleeptime=$((sleeptime + 1))
                         #echo "${i} has been sleeping for ${sleeptime} seconds"
                         if [ "$sleeptime" -gt "$allowedsleep" ]; then
                                 echo "Killed proccess: ${i} as it has been sleeping for more than ${allowedsleep} seconds"; mysql -e "kill ${i}";
@@ -16,13 +17,13 @@ function kill_mysql_sleeping_proc() {
                                 prockilled=$((prockilled + 1));
                         fi
                 done
-		if [ "$prockilled" -eq 0 ]; then
-			echo "No quries have been running for more than $allowedsleep"
-		elif [ "$prockilled" -eq 1 ]; then
-			echo "Killed only 1 MySQL query that was sleeping for more than $allowedsleep"
-	        else
-		        echo "Killed ${prockilled} long sleeping MySQL queries"
-		fi
+                if [ "$prockilled" -eq 0 ]; then
+                        echo "No quries have been running for more than $allowedsleep"
+                elif [ "$prockilled" -eq 1 ]; then
+                        echo "Killed only 1 MySQL query that was sleeping for more than $allowedsleep"
+                else
+                    	echo "Killed ${prockilled} long sleeping MySQL queries"
+                fi
         fi
 }
-kill_mysql_sleeping_proc
+kill_mysql_sleeping_pro
