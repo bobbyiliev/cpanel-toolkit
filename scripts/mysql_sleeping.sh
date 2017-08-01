844166b26a9d4533884c9153b6753893e22c1823
root <root@bobby.servers.prgn.misp.co.uk>
2017-08-01 10:14:49 +0000

    fixing a syntax error

--- scripts/mysql_sleeping.sh
+++ scripts/mysql_sleeping.sh
@@ -7,23 +7,27 @@ function kill_mysql_sleeping_proc() {
         echo "No Sleeping MySQL Proccesses ATM";
         else
             	for i in $(mysql -e 'show processlist' | grep 'Sleep' | awk '{print $1}'); do
-                        prockilled=0
+                        #declare -i prockilled=0
                         sleeptime=$(mysqladmin proc | grep "\<$i\>" | grep -v '\-\-' | grep -v 'Time' | awk -F'|' '{ print $7 }' | sed 's/ //g' | tail -1);
                         sleeptime=$((sleeptime + 1))
                         #echo "${i} has been sleeping for ${sleeptime} seconds"
                         if [ "$sleeptime" -gt "$allowedsleep" ]; then
                                 echo "Killed proccess: ${i} as it has been sleeping for more than ${allowedsleep} seconds"; mysql -e "kill ${i}";
                                 #echo "$i has been sleeping for $sleeptime seconds"
-                                prockilled=$((prockilled + 1));
+                                prockilled=$((prockilled+1));
                         fi
                 done
-                if [ "$prockilled" -eq 0 ]; then
-                        echo "No quries have been running for more than $allowedsleep"
-                elif [ "$prockilled" -eq 1 ]; then
-                        echo "Killed only 1 MySQL query that was sleeping for more than $allowedsleep"
-                else
-                    	echo "Killed ${prockilled} long sleeping MySQL queries"
+                if [ ! -z $prockilled ] && [ $prockilled -lt 1 ]; then
+                        echo "No quries have been running for more than $allowedsleep seconds"
+                elif [ ! -z $prockilled ] && [ $prockilled -eq 1 ]; then
+                        echo "Killed only 1 MySQL query that was sleeping for more than $allowedsleep seconds"
+                elif [ ! -z $prockilled ] && [ $prockilled -gt 1 ]; then
+                        echo "Killed $prockilled MySQL query that was sleeping for more than $allowedsleep seconds"
+                else {
+                      	echo "No quries have been sleeping for more than $allowedsleep seconds"
+                }
                 fi
         fi
 }
-kill_mysql_sleeping_pro
+kill_mysql_sleeping_proc
+
