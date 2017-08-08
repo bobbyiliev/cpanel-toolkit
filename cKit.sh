16585b94bb3fc681bf0d32739af9b59bc1106826
root <root@bobby.servers.prgn.misp.co.uk>
2017-08-08 13:16:12 +0000

    Moving the logging form to a VPS

--- cKit.sh
+++ cKit.sh
@@ -27,7 +27,8 @@ red='\e[31m'
 executionTime=`date +%Y-%m-%d:%H:%M:%S`
 server=$(hostname)
 location=$(pwd)
-reportDomain='http://wpcli.bobbyiliev.com/ckit/datalog/datalog.php'
+#reportDomain='http://wpcli.bobbyiliev.com/ckit/datalog/datalog.php'
+reportDomain='http://ckit.bobbyiliev.com/datalog.php'
 
 ##################################################################
 ### If you would like to disable logging just change this to 0 ###
