415138e61f45ce7b2692836421ed0cb2eb33f13d
root <root@bobby.servers.prgn.misp.co.uk>
2017-08-09 11:29:39 +0000

    minor changes

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
+###################
+###  Variables  ###
+###################
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+red='\e[31m'
+executionTime=`date +%Y-%m-%d:%H:%M:%S`
+server=$(hostname)
+location=$(pwd)
+#reportDomain='http://wpcli.bobbyiliev.com/ckit/datalog/datalog.php'
+reportDomain='http://ckit.bobbyiliev.com/datalog.php'
+
+##################################################################
+### If you would like to disable logging just change this to 0 ###
+##################################################################
+enablelog=1
+###################
+###  Functions  ###
+###################
+ColorGreen(){
+	echo -ne $green$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
+ColorRed(){
+	echo -ne $red$1$clear
+}
 
 function welcome() {
-	echo "### Some Useful Information ###"
+	echo $(ColorGreen '### Some Useful Information ###' )
 
-	echo "The server is using about $(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}') of its CPU  Power"
+	echo "The server is using about $(ColorGreen "$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')" ) of its CPU  Power"
 
-	echo "Total of $(free -mh | grep Mem | awk '{ print $2 }') RAM installed"
+	echo "Total of $(ColorGreen "$(free -mh | grep Mem | awk '{ print $2 }') ") RAM installed"
 
-	echo "Total of $(lscpu | grep -v 'node' | grep 'CPU(s):' | awk '{ print $2 }') CPU(s)"
+	echo "Total of $(ColorGreen "$(lscpu | grep -v 'node' | grep 'CPU(s):' | awk '{ print $2 }')") CPU(s)"
 
 	echo "OS: $(cat /etc/redhat-release)"
 
-	echo "If CentOS is under version 6, EasyApache 4 and Let's Encrypt can not be installed"
+	echo "If CentOS is under version 6, EasyApache 4 and Let's Encrypt can NOT be installed"
 }
 
 welcome
