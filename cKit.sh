6ecdeb35b0f325194c2a50737cac7209421076e8
root <root@alex.servers.prgn.misp.co.uk>
2017-08-09 16:00:50 +0000

    Adding ioncube installer to cKit.sh

--- cKit.sh
+++ cKit.sh
@@ -2237,6 +2237,82 @@ ChangePHPVersion
 }
 
 
+##
+# Function that installs ioncube for PHP 7 on the Cloud Platform
+# Use it when you need the lastest version of ioncube
+##
+
+function install_ioncube_php70() {
+
+        if ! grep -q "AddType x-httpd-php7" ~/.htaccess 2>/dev/null ; then
+                echo $(ColorGreen  "This is only for PHP 7.0, and you are running a different PHP version!")
+	CloudMenu
+        fi
+
+        whichletter="$(pwd | awk -F/ '{print $4}')"
+        whichdomain="$(pwd | awk -F/ '{print $5}')"
+        PHP_VER="$(if grep -q "AddType x-httpd-php7" ~/.htaccess 2>/dev/null ; then
+           echo "7.0";
+        fi
+        )"
+
+
+        # Function to Download and extract 64-bit files
+
+        clear
+            echo "$(ColorRed 'Downloading the ioncube archieve')";
+                echo ""
+            wget -q https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+
+
+        # Extract the archieve
+                sleep 2s
+            echo "$(ColorGreen 'Extracting archieve')";
+            echo ""
+                tar xvf ioncube_loaders_lin_x86-64.tar.gz
+
+        # Creating php.ini
+
+        if grep -q "AddType x-httpd-php7 .php" ~/.htaccess 2>/dev/null
+        then
+                echo ""
+            echo "$(ColorGreen 'The current PHP version is 7')
+                           ";
+                           cd ~/public_html/
+                           mv php.ini php.ini-old 2>/dev/null
+                           cd
+                        mv php.ini php.ini-old 2>/dev/null
+                        wget -q http://paragon.alexgeorgiev.net/phpini/php.ini-7
+                           echo -ne "$(ColorGreen '- Creating a new optimized php.ini file. Memory_limit has been set to 1024M, max_execution_time has been set to 900, max_input_vars has been set to 8000 
+and
+   	error_logging$
+        created.')
+        ";
+
+                        mv php.ini-7 php.ini
+                if grep -q "suPHP_ConfigPath /var/sites/${whichletter}/${whichdomain}/public_html/.htaccess" ~/public_html/.htaccess 2>/dev/null
+            then
+                           echo -ne "$(ColorGreen '- There is a valid suPHP_ConfigPath in public_html/.htaccess-skipping')
+        ";
+            else
+                           echo -ne "$(ColorGreen "- Couldn't find a valid SuPHP_ConfigPath, creating a new one in public_html/.htaccess")
+        ";
+                        echo -e "suPHP_ConfigPath /var/sites/${whichletter}/${whichdomain}/php.ini\n$(cat ~/public_html/.htaccess 2>/dev/null)" > ~/public_html/.htaccess 2>/dev/null
+                   fi
+        fi
+
+
+        # Add ioncube to php.ini
+
+            echo ""
+            echo "$(ColorGreen 'Adding ioncube to the php.ini')";
+            echo "" >> ~/php.ini 2>/dev/null
+            echo -e "zend_extension_ts=/var/sites/${whichletter}/${whichdomain}/ioncube/ioncube_loader_lin_${PHP_VER}_ts.so" >> ~/php.ini 2>/dev/null
+            echo -e "zend_extension=/var/sites/${whichletter}/${whichdomain}/ioncube/ioncube_loader_lin_${PHP_VER}.so" >> ~/php.ini 2>/dev/null
+            echo ""
+}
+install_ioncube_php70
+
 ###########################
 ###  Quick Access Menu  ###
 ###########################
@@ -2297,6 +2373,7 @@ $(ColorGreen '2)') PHP configurations and settigs
 $(ColorGreen '3)') Check if a PHP extension is enabled on the server.
 $(ColorGreen '4)') Check if a PHP function is enabled on the server.
 $(ColorGreen '5)') Generate random password
+$(ColorGreen '6)') Install Ioncube for a website using PHP 7
 $(ColorGreen '0)') Back to Main Menu
 
 $(ColorBlue 'Choose an option:') "
@@ -2307,6 +2384,7 @@ $(ColorBlue 'Choose an option:') "
                 3) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=IsExtensionEnabled\&Server=$server\&Path=$location ; fi ; is_extension;;
                 4) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=IsFunctionnEnabled\&Server=$server\&Path=$location ; fi ; is_function;;
 		5) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=RandomPass\&Server=$server\&Path=$location ; fi ; randompass_cloud;;
+		6) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=RandomPass\&Server=$server\&Path=$location ; fi ; install_ioncube_php70;;
 		0) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=MainMenu\&Server=$server\&Path=$location ; fi ; MainMenu;;
                 *) echo -e $red"Wrong command."$clear; CloudMenu;;
         esac
