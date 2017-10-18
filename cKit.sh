eca1a79d29fe483b0ea797856db06d3957779605
root <root@alex.servers.prgn.misp.co.uk>
2017-10-18 13:36:02 +0100

    Adding function for outdated wordpress websites

--- cKit.sh
+++ cKit.sh
@@ -3361,6 +3361,97 @@ $(ColorGreen "It seems the issue was with the theme. Going back to the menu")";
 }
 
 
+##
+# Functions that search for outdated WordPress installations in /home/*
+# You can use it in order to located outdated websites, which are vulnerable and further email the clients if you wish.
+##
+
+#!/bin/bash
+
+function find_outdated_wp() {
+        #--Activate output via email by setting "sendmail" to 1 and uncomment "email"--#
+        sendmail=0
+        #email=user@yourdomain.com
+
+        echo "**************************Wordpress Outdated Installation Finder*****************************"
+        echo ""
+
+        output=/tmp/wp_outdated.txt
+        tempf1=/tmp/search_wp
+        tempf2=/tmp/temp_output
+        conf=/usr/local/apache/conf/httpd.conf
+
+        rm -f $output
+        rm -f $tempf1
+        rm -f $tempf2
+
+        #read -e -p "Enter the directory path to scan (Eg: /home, /home/*/public_html): "  dir
+
+        wp_latest=$( curl -k -s "https://api.wordpress.org/core/version-check/1.6/" | awk -F'"' {'print $10'} | cut -d"-" -f2 | cut  -c -5 )
+        echo "WordPress latest Version is $wp_latest"
+    echo "Search in progress, this may take several minutes"
+    echo ""
+        ionice -n 3 -c 3 2>/dev/null find /home ! -path "/home/virtfs/*" -type f -iwholename "*/wp-includes/version.php" -exec grep -H "\$wp_version =" {} >> $tempf1 \; &
+
+    wait
+    sleep 2
+
+        echo "Website URL | Username | Full Path | Version"  >> $tempf2
+        echo "----------------- | -------- | --------- | -------" >> $tempf2 
+        for j in $( sed 's/ //g' $tempf1 )
+        do
+        path=$( echo  "$j" | awk -F"wp-includes" {'print $1'} | sed 's/.$//' )
+        nf=$( echo $path | grep -o "/" | wc -l )
+        ver=$( echo "$j" | cut -d\' -f2 )
+        user=$( echo "$path" | cut -d"/" -f3 )
+        lastf=""
+
+                if [ "$(printf "$wp_latest\n$ver" | sort -V | head -n1)" == "$ver" ] && [ "$ver" != "$wp_latest" ] ; then
+                        ct=0
+                        path_org=$path
+                        while [ $nf -gt 2 ] ;do
+                                if  grep -q -B2 $path $conf ;then
+                                dom=$( grep  -B2 $path $conf | head -n1 | awk  {'print $2'} )
+                                echo  "$dom$lastf | $user | $path_org | $ver" >>  $tempf2
+                                break
+                                else
+                                (( ct++ ))
+                                lastf=/$( echo "$path_org" | rev | cut -d"/"  -f1-$ct | rev )
+                                path=$( echo "$path" | rev | cut -d"/" -f2- | rev )
+                                (( nf-- ))
+                                fi
+                        done
+                else
+                sleep 1
+                fi
+        done
+        echo ""
+        if  sed '3q;d' $tempf2 | grep -E -q "*" ; then
+        cat $tempf2 | column -s '|' -s '|' -t > $output
+        #echo "Search completed. Find the Output file at: $output "
+    echo "Search completed"
+    cat $output
+    rm -f $output
+    rm -f $tempf1
+    rm -f $tempf2
+
+        #Send output via email
+         if [ $sendmail -eq 1 ];then
+        echo "Please find the attachement" | mail -s "WordPress Outdated installations" -a $output $email 
+        echo "Output has been sent to $email"
+         else
+         sleep 1
+         fi
+
+        else
+        echo "No outdated Wordpress installation found"
+        fi
+        echo ""
+        exit 0
+}
+
+find_outdated_wp
+
 ###########################
 ###  Quick Access Menu  ###
 ###########################
@@ -3711,6 +3802,7 @@ $(ColorGreen '3)') Generate a random password
 $(ColorGreen '4)') Live Monitor of the CPU.
 $(ColorGreen '5)') Find files larger than 100MB in /home/
 $(ColorGreen '6)') Check the EasyApache Version
+$(ColorGreen '7)') Find Outdated WordPress installations
 $(ColorGreen '0)') Back To Main Menu.
 
 $(ColorBlue 'Choose an option:') "
@@ -3722,7 +3814,8 @@ $(ColorBlue 'Choose an option:') "
 		4) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=MonitorCPU\&Server=$server\&Path=$location ; fi ; MonitorCpu;;
 		5) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=FindLargeFiles\&Server=$server\&Path=$location ; fi ; FindLargeFiles;;
 		6) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=EAversion\&Server=$server\&Path=$location ; fi ; EAversion;;
-                0) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=MainMenu\&Server=$server\&Path=$location ; fi ; MainMenu;;
+                7) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=CheckOudatedWP\&Server=$server\&Path=$location ; fi ; find_outdated_wp;;
+		0) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=MainMenu\&Server=$server\&Path=$location ; fi ; MainMenu;;
 		*) echo -e $red"Wrong command."$clear; ToolsMenu;;
         esac
 fi
