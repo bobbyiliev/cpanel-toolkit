846ee6cf351e469ee99e1d02bf3d5bf4f3765cd1
Bobby I <bobby@bobbyiliev.com>
2017-11-29 16:23:45 +0000

    Added a new useful exim function - summury of the whole exim log

--- cKit.sh
+++ cKit.sh
@@ -433,6 +433,53 @@ function showeximsum(){
 EmailsMenu
 }
 
+
+##
+# Function that summarizes the whole exim log! Very useful.
+##
+
+function summarize_exim_log() {
+
+	echo "This might take a while.. Please be patient.."
+	echo ""
+	echo "Here's the top 10 senders of messages via script since 12AM this morning:"
+	/bin/sed -ne "s|$(date +%F).*cwd=\(/home[^ ]*\).*$|\1|p" /var/log/exim_mainlog | sort | uniq -c | awk '{printf "%05d %s\n",$1,$2}' | sort | tail -n 10
+
+	echo ""
+	echo "And the top 10 script senders yesterday:"
+	/bin/sed -ne "s|$(date +%F --date="1 day ago").*cwd=\(/home[^ ]*\).*$|\1|p" /var/log/exim_mainlog | sort | uniq -c | awk '{printf "%05d %s\n",$1,$2}' | sort | tail -n 10
+
+	echo ""
+	echo "Here's the top 10 senders via direct SMTP AUTH since 12AM this morning:"
+	/usr/local/bin/perl -lsne '/$today.* \[([0-9.]+)\]:.+dovecot_(?:login|plain):([^\s]+).* for (.*)/ and $sender{$2}{r}+=scalar (split / /,$3) and $sender{$2}{i}{$1}=1; END {foreach $sender(keys %sender){printf"%05d Hosts=%03d Auth=%s\n",$sender{$sender}{r},scalar (keys %{$sender{$sender}{i}}),$sender;}}' -- -today=$(date +%F) /var/log/exim_mainlog | sort | tail -n 10
+
+	echo ""
+	echo "And the top 10 SMTP AUTH senders yesterday:"
+	/usr/local/bin/perl -lsne '/$today.* \[([0-9.]+)\]:.+dovecot_(?:login|plain):([^\s]+).* for (.*)/ and $sender{$2}{r}+=scalar (split / /,$3) and $sender{$2}{i}{$1}=1; END {foreach $sender(keys %sender){printf"%05d Hosts=%03d Auth=%s\n",$sender{$sender}{r},scalar (keys %{$sender{$sender}{i}}),$sender;}}' -- -today=$(date +%F --date="1 day ago") /var/log/exim_mainlog | sort | tail -n 10
+
+
+	echo ""
+	echo "Here are the top 10 users sending mail via local SMTP since 12AM this morning (identify_local_connection):"
+	grep $(date +%F) /var/log/exim_mainlog | grep "identify_local_connection" | grep -v -e root -e mailman -e mailnull | awk '{print $9}' | cut -d"=" -f2 | sort | uniq -c | awk '{printf "%05d %s\n",$1,$2}' | sort | tail -n 10
+
+	# List the email forwarders that are forwarding the most
+	echo ""
+	echo "Here are the top 10 email forward destinations (this takes a while):"
+	forwarders=($(for i in /etc/valiases/*; do grep -v -e mailman -e fail $i | cut -d':' -f2- | tr ',' $'\n' | sed 's/ //' | grep -v ".autorespond" ; done | sort | uniq))
+	forwarder_total=${#forwarders[@]}
+	forwarder_count=()
+	for (( j = 0 ; j<=$(($forwarder_total-1)) ; j++ )); do forwarder_count[$j]=$(grep "=> ${forwarders[$j]}" /var/log/exim_mainlog | wc -l | awk '{printf "%05d %s\n",$1,$2}'); done
+	for (( k = 0 ; k<=$(($forwarder_total-1)) ; k++ )); do echo ${forwarder_count[$k]} ${forwarders[$k]} ; done | sort -n | tail
+
+	echo ""
+	echo "Here's a list of processes connected to Exim. They ***MAY*** be spamming."
+	/usr/sbin/lsof -i | grep smtp | grep -v exim
+	echo "If that was empty, it means there aren't any we're done here in any case."
+wait
+EmailsMenu
+}
+
+
 ##
 # Startup MySQL Info
 ##
@@ -3720,13 +3767,14 @@ echo -ne "
 Choose the information you need regarding Email Logs
 
 $(ColorGreen '1)') Receive a sorted list of all the email senders in the exim mail queue.
-$(ColorGreen '2)') This option will display the directories from which the emails are being sent.
-$(ColorGreen '3)') This option will check for emails sent via php script.
-$(ColorGreen '4)') This option will display the users which were sending out emails within their directories.
-$(ColorGreen '5)') It shows the IPs which were sending emails via port 25..
-$(ColorGreen '6)') In order to find “nobody” spamming, use this option..
+$(ColorGreen '2)') Display the directories from which the emails are being sent.
+$(ColorGreen '3)') Check for emails sent via php script.
+$(ColorGreen '4)') Display the users which were sending out emails within their directories.
+$(ColorGreen '5)') List the IPs which were sending emails via port 25.
+$(ColorGreen '6)') List users spamming as "nobody".
 $(ColorGreen '7)') The above option is valid only if the spamming is currently in progress If not use this otpion..
 $(ColorGreen '8)') Summary of the mails in the mail queue.
+$(ColorGreen '9)') Summary of the whole exim log - Very Useful!!!
 $(ColorGreen '0)') Back to Main Menu.
 
 $(ColorBlue 'Choose an option:') "
@@ -3740,6 +3788,7 @@ $(ColorBlue 'Choose an option:') "
                 6) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=Exim6NobodySpam\&Server=$server\&Path=$location ; fi ; nobodyspam;;
                 7) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=Exim7SpamInProgress\&Server=$server\&Path=$location ; fi ; nobodyspamafter;;
                 8) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=Exim8MailQueue\&Server=$server\&Path=$location ; fi ; showeximsum;;
+                9) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=SummaryEximLog\&Server=$server\&Path=$location ; fi ; summarize_exim_log;;
                 0) if [[ $enablelog == 1 ]] ; then curl ${reportDomain}?user=$paruser\&Date=$executionTime\&Executed=MainMenu\&Server=$server\&Path=$location ; fi ; MainMenu;;
 		*) echo -e $red"Wrong command."$clear; EmailsMenu;;
         esac
